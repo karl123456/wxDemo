7ffc916ac824df052620e201e3a93769f71150e9
chenpf123456 <1578293861@qq.com>
2019-04-16 11:01:16 +0800

    测试

--- pages/index/index.js
+++ pages/index/index.js
@@ -28,7 +28,7 @@ Page({
         this.setData({
           userInfo: res.userInfo,
           hasUserInfo: true
-        })
+        });
       }
     } else {
       // 在没有 open-type=getUserInfo 版本的兼容处理
